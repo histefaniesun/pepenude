129b134955e10482ec305543a34abc4ebdfbc589
DeepManYY <deepmanyy@msgsafe.io>
2019-07-17 22:09:58 -0500

    Is DreamTime!

--- src/gui/components/Nudity/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import NudityPreview from './Preview'
-import NudityUpload from './Upload'
-
-Vue.component('nudity-preview', NudityPreview)
-Vue.component('nudity-upload', NudityUpload)
--- src/gui/modules/models/model.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash'
-
-export default class Model {
-  constructor(id) {
-    this.id = id
-  }
-}
